a94e709dc3605a1320441d2b075408b81304845a
Greg Lincoln <greg@greglincoln.com>
2017-09-13 22:33:23 -0400

    Rework menu manager to deal with scrolling. Add menuItem. (needs a clean API)

--- src/MenuManager.h
+++ src/MenuManager.h
@@ -8,7 +8,7 @@
 const uint8_t TEMP_SYMBOL = 0x02, UP_ARROW = 0x03;
 
 class MenuManager {
-    const unsigned long EncoderTickLength = 30;
+    const unsigned long EncoderTickLength = 20;
 
     void _up() {
         digitalWrite(encoder_1, LOW);
@@ -55,11 +55,12 @@ public:
         auto thing = timer.defer(100, [] { digitalWrite(encoder_switch, HIGH); })
                 .then(100, [&] {
                     clickGoesUp ? menuLevel-- : menuLevel++;
+                    moved = 0;
                 });
     }
 
     void up() {
-        if(menuEncoderReversed) {
+        if (menuEncoderReversed) {
             _down();
         } else {
             _up();
@@ -67,11 +68,15 @@ public:
     }
 
     void down() {
-        if(menuEncoderReversed) {
+        if (menuEncoderReversed) {
             _up();
         } else {
             _down();
         }
+    }
+
+    void down_until(string match) {
+
     }
 
 private:
@@ -82,7 +87,8 @@ private:
 
     uint8_t menuLevel = 0;
     bool clickGoesUp = false;
-    uint8_t menuRow = 0;
+    int8_t moved = 0;
+    string menuItem = "";
 
     void attachUpdate() {
         screenBuffer.subUpdate([&](ScreenBuffer *) {
@@ -90,22 +96,39 @@ private:
             uint8_t offsets[] = {0, 20, 40, 60};
             if (buffer[0] == TEMP_SYMBOL) {
                 menuLevel = 0;
+                menuItem = "";
                 return;
             }
             for (auto offset: offsets) {
 //                Serial.printf("Offset: %u, Buffer: %2X\r\n", offset, buffer[offset]);
-                if (buffer[offset] == UP_ARROW) {
-                    clickGoesUp = true;
-                    menuRow = 1;
-                    break;
-                } else if (buffer[offset] == '>') {
-                    clickGoesUp = false;
-                    menuRow = static_cast<uint8_t>(offset / 20 + 1);
+                if (buffer[offset] != ' ') {
+                    clickGoesUp = buffer[offset] == UP_ARROW;
+                    static uint8_t priorRow{};
+                    uint8_t newRow = static_cast<uint8_t>(offset / 20 + 1);
+                    string newMenuItem = {buffer.begin() + offset + 1, buffer.begin() + offset + 20};
+
+                    // if we're at the bottom or top, we might be scrolling which is "moving"
+                    if (newRow == 4 || newRow == 1) {
+                        if (newMenuItem != menuItem) {
+                            // we scrolled, so that's moving
+                            if (newRow == 4) {
+                                moved = 1;
+                            } else {
+                                moved = -1;
+                            }
+                        } else {
+                            moved = 0;
+                        }
+                    } else { // normal moving (i.e. not scrolling)
+                        moved = newRow - priorRow;
+                    }
+                    priorRow = newRow;
+                    menuItem = std::move(newMenuItem);
                     break;
                 }
             }
 
-            Serial.printf("Menu row: %u", menuRow);
+            Serial.printf("Menu item: %s, moved: %d", menuItem.c_str(), moved);
         });
     }
 };
